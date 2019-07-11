ed00a02309bbcbd8b07876def7dcd5a3a35bf36b
Chris Bushman <christopherwbushman@gmail.com>
2019-07-11 17:44:23 -0500

    Adding test project I did
    This is part of a UI tutorial I'm working on. Question is, what files do I really want to version?

---
+++ Assets/RW/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Animator startButton;
+    public Animator settingsButton;
+    public void StartGame()
+    {
+        SceneManager.LoadScene("RocketMouse");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+    }
+}
